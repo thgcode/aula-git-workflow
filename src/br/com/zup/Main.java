c4182e87b45a8ea7b45c928c6db08f39f677e6ef
Thiago Seus <thiago.seus@yahoo.com.br>
2021-03-01 14:22:44 -0300

    Criar função de imprimir aluno

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -2,6 +2,10 @@ package br.com.zup;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.println("Os alunos cadastrados no sistema foram:");
+        for (Aluno aluno: Aluno.values()) {
+            System.out.println(aluno.name());
+        }
     }
+
 }
